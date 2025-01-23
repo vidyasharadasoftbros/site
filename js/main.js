ce1008b126a6f3be01688966cc50550c9ac117ee
softbrosin <softbros.web@gmail.com>
2025-01-23 14:06:45 +0530

    latest

---
+++ js/main.js
@@ -0,0 +1,271 @@
+/* =====================================
+Template Name: 	Mediplus.
+Author Name: Naimur Rahman
+Website: http://wpthemesgrid.com/
+Description: Mediplus - Doctor HTML Template.
+Version:	1.1
+========================================*/
+/*=======================================
+[Start Activation Code]
+=========================================
+* Sticky Header JS
+* Search JS
+* Mobile Menu JS
+* Hero Slider JS
+* Testimonial Slider JS
+* Portfolio Slider JS
+* Clients Slider JS
+* Single Portfolio Slider JS
+* Accordion JS
+* Nice Select JS
+* Date Picker JS
+* Counter Up JS
+* Checkbox JS
+* Right Bar JS
+* Video Popup JS
+* Wow JS
+* Scroll Up JS
+* Animate Scroll JS
+* Stellar JS
+* Google Maps JS
+* Preloader JS
+=========================================
+[End Activation Code]
+=========================================*/
+(function ($) {
+    "use strict";
+    $(document).on('ready', function () {
+
+        jQuery(window).on('scroll', function () {
+            if ($(this).scrollTop() > 200) {
+                $('#header .header-inner').addClass("sticky");
+            } else {
+                $('#header .header-inner').removeClass("sticky");
+            }
+        });
+
+        /*====================================
+            Sticky Header JS
+        ======================================*/
+        jQuery(window).on('scroll', function () {
+            if ($(this).scrollTop() > 100) {
+                $('.header').addClass("sticky");
+            } else {
+                $('.header').removeClass("sticky");
+            }
+        });
+
+        /*====================================
+            Search JS
+        ======================================*/
+        $('.search a').on("click", function () {
+            $('.search-top').toggleClass('active');
+        });
+
+        /*====================================
+            Mobile Menu
+        ======================================*/
+        $('.menu').slicknav({
+            prependTo: ".mobile-nav",
+            duration: 300,
+            closeOnClick: true,
+        });
+
+        /*===============================
+            Hero Slider JS
+        =================================*/
+        $(".hero-slider").owlCarousel({
+            loop: true,
+            autoplay: true,
+            smartSpeed: 500,
+            autoplayTimeout: 3500,
+            singleItem: true,
+            autoplayHoverPause: true,
+            items: 1,
+            nav: true,
+            navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+            dots: false,
+        });
+
+        /*===============================
+            Testimonial Slider JS
+        =================================*/
+        $('.testimonial-slider').owlCarousel({
+            items: 3,
+            autoplay: true,
+            autoplayTimeout: 4500,
+            smartSpeed: 300,
+            autoplayHoverPause: true,
+            loop: true,
+            merge: true,
+            nav: false,
+            dots: true,
+            responsive: {
+                1: {
+                    items: 1,
+                },
+                300: {
+                    items: 1,
+                },
+                480: {
+                    items: 1,
+                },
+                768: {
+                    items: 2,
+                },
+                1170: {
+                    items: 3,
+                },
+            }
+        });
+
+        /*===============================
+            Portfolio Slider JS
+        =================================*/
+        $('.portfolio-slider').owlCarousel({
+            autoplay: true,
+            autoplayTimeout: 4000,
+            margin: 15,
+            smartSpeed: 300,
+            autoplayHoverPause: true,
+            loop: true,
+            nav: true,
+            dots: false,
+            responsive: {
+                300: {
+                    items: 1,
+                },
+                480: {
+                    items: 2,
+                },
+                768: {
+                    items: 2,
+                },
+                1170: {
+                    items: 4,
+                },
+            }
+        });
+        /*===============================
+            Clients Slider JS
+        =================================*/
+        $('.clients-slider').owlCarousel({
+            items: 5,
+            autoplay: true,
+            autoplayTimeout: 3500,
+            margin: 15,
+            smartSpeed: 400,
+            autoplayHoverPause: true,
+            loop: true,
+            nav: false,
+            dots: false,
+            responsive: {
+                300: {
+                    items: 1,
+                },
+                480: {
+                    items: 2,
+                },
+                768: {
+                    items: 3,
+                },
+                1170: {
+                    items: 5,
+                },
+            }
+        });
+
+        /*====================================
+            Single Portfolio Slider JS
+        ======================================*/
+        $('.pf-details-slider').owlCarousel({
+            items: 1,
+            autoplay: false,
+            autoplayTimeout: 5000,
+            smartSpeed: 400,
+            autoplayHoverPause: true,
+            loop: true,
+            merge: true,
+            nav: true,
+            dots: false,
+            navText: ['<i class="icofont-rounded-left"></i>', '<i class="icofont-rounded-right"></i>'],
+        });
+
+        /*===================
+            Accordion JS
+        =====================*/
+        $('.accordion > li:eq(0) a').addClass('active').next().slideDown();
+        $('.accordion a').on('click', function (j) {
+            var dropDown = $(this).closest('li').find('p');
+            $(this).closest('.accordion').find('p').not(dropDown).slideUp(300);
+            if ($(this).hasClass('active')) {
+                $(this).removeClass('active');
+            } else {
+                $(this).closest('.accordion').find('a.active').removeClass('active');
+                $(this).addClass('active');
+            }
+            dropDown.stop(false, true).slideToggle(300);
+            j.preventDefault();
+        });
+
+        /*===============================
+            Checkbox JS
+        =================================*/
+        $('input[type="checkbox"]').change(function () {
+            if ($(this).is(':checked')) {
+                $(this).parent("label").addClass("checked");
+            } else {
+                $(this).parent("label").removeClass("checked");
+            }
+        });
+
+        /*===============================
+            Right Bar JS
+        =================================*/
+        $('.right-bar .bar').on("click", function () {
+            $('.sidebar-menu').addClass('active');
+        });
+        $('.sidebar-menu .cross').on("click", function () {
+            $('.sidebar-menu').removeClass('active');
+        });
+    });
+
+    /*====================
+        Preloader JS
+    ======================*/
+    $(window).on('load', function () {
+        $('.preloader').addClass('preloader-deactivate');
+    });
+
+
+})(jQuery);
+
+const sections = document.querySelectorAll("section:not(.no-anim)");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("in-view");
+        } else {
+            entry.target.classList.remove("in-view");
+        }
+    });
+});
+
+sections.forEach((section) => observer.observe(section));
+
+// Wa Plugin 
+
+document.body.insertAdjacentHTML('beforeend', '<a class="whatsapp-plug" href="https://wa.me/918147121649"></a>')
+
+document.querySelectorAll('.custom-cards').forEach(card => {
+    card.querySelector('.card-summary a').addEventListener('click', (e) => {
+        e.preventDefault();
+        document.body.style.overflowY = 'hidden';
+        card.querySelector('.hidden-card-body').classList.add('active');
+        card.querySelector('.hidden-card-body span').addEventListener('click', () => {
+            card.querySelector('.hidden-card-body').classList.remove('active');
+            document.body.style.overflowY = 'unset';
+        });
+    });
+})
